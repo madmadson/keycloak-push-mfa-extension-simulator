43a30655503244e6051526d5a62508d1da780b69
Tobias Matt <matttobi@googlemail.com>
2025-12-15 16:29:35 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # keycloak-push-mfa-extension-simulator
+
 Simulator for Keycloak push MFA Extension
+
+
+## start local
+
+mvn spring-boot:run
---
+++ src/main/java/de/arbeitsagentur/pushmfasim/controller/InfoController.java
@@ -0,0 +1,17 @@
+package de.arbeitsagentur.pushmfasim.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class InfoController {
+
+
+    @GetMapping(path = "/info")
+    public String showInfoPage() {
+        return "info-page";
+    }
+
+}
